0f64d53337daab5baf3a9d78c57e3a0a0ac0c49a
svkashini <koki.kashihara@gmail.com>
2021-09-22 22:04:48 +0900

    first commit

---
+++ suzuki_covid.py
@@ -0,0 +1,169 @@
+#ライブラリのインポート
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import streamlit as st
+
+import matplotlib.pyplot as plt
+import plotly.express as px
+import pydeck as pdk
+
+####
+#ここからデータの取得
+####
+
+#スズキ各拠点の緯度経度情報
+location = pd.DataFrame([
+    ['本社（静岡県浜松市南区）', 34.692419958984466, 137.68810529817713],
+    ['工場（静岡県湖西市）', 34.70056416105026, 137.5044475374429],
+    ['事業所（静岡県湖西市）', 34.70056416105026, 137.5044475374429],
+    ['工場（静岡県牧之原市）', 34.73401794429481, 138.17318891352258],
+    ['事業所（静岡県牧之原市）', 34.73401794429481, 138.17318891352258],
+    ['工場（静岡県磐田市）', 34.742965335849085, 137.87492658865304],
+    ['工場（静岡県浜松市北区）', 34.80683127454381, 137.73895557490206],
+    ['事業所（静岡県浜松市北区）', 34.80683127454381, 137.73895557490206],
+    ['工場（静岡県掛川市）', 34.68886908396566, 137.98902904421084],
+    ['事業所（東京都練馬区）', 35.6630485838581, 139.75786500182355]
+], columns=['拠点', 'lat', 'long'])
+
+
+#スズキ株式会社のコロナ感染者公表ページ
+url = 'https://www.suzuki.co.jp/release/d/2020/covid19/'
+res = requests.get(url)
+
+#データの抜き出し
+html_doc = res.content
+soup = BeautifulSoup(html_doc, 'html.parser')
+
+#感染者情報の表を読み込み
+div_box = soup.find_all('div', class_='boxLine boxTable')
+
+#Beautiful soupで取得したデータをpandas DataFrameに格納する
+
+df = pd.DataFrame() #空のデータフレームを準備
+
+#データフレームのカラム名を辞書で定義
+columns = {0:'拠点',
+           1:'陽性確定',
+           2:'最終出社',
+           3:'職場消毒',
+           4:'工場稼働への影響',
+           5:'特記事項'}
+
+#各要素をデータフレームに格納
+for i in div_box:
+    temp = []
+    for x in i.find_all(['th', 'td']):
+        if x.name == 'td':
+            temp.append(x.get_text())
+    df_temp = pd.DataFrame(temp).T
+    
+    df = pd.concat([df, df_temp], axis=0)
+
+#不要なインデックスを削除し、カラム名を変更する
+suzuki_covid = df.reset_index().drop(columns='index', axis=1).rename(columns=columns)
+
+#2020年の公表データには月日しか記載がないため、2021年データと書式を揃えるために「2020年」を追加するスクリプト
+temp = suzuki_covid[~suzuki_covid['陽性確定'].str.contains('年')]
+temp['陽性確定'] = temp['陽性確定'].apply(lambda x: '2020年' + x )
+temp1 = suzuki_covid[suzuki_covid['陽性確定'].str.contains('年')]
+suzuki_covid2 = pd.concat([temp1, temp], axis=0)
+
+
+#相良クラスターの別カウント
+#9/14確認数
+cluster = 28
+
+for i in range(cluster):
+    df_cluster = pd.DataFrame.from_dict({
+        '拠点':['工場（静岡県牧之原市）'],
+        '陽性確定':['2021年9月14日'],
+        '工場稼働への影響': ['あり'],
+        '特記事項':['クラスター']
+        }
+    )
+
+    suzuki_covid2 = pd.concat([suzuki_covid2, df_cluster], axis=0)
+
+
+#9/15確認数
+cluster = 30
+
+for i in range(cluster):
+    df_cluster = pd.DataFrame.from_dict({
+        '拠点':['工場（静岡県牧之原市）'],
+        '陽性確定':['2021年9月15日'],
+        '工場稼働への影響': ['あり'],
+        '特記事項':['クラスター']
+        }
+    )
+
+    suzuki_covid2 = pd.concat([suzuki_covid2, df_cluster], axis=0)
+    
+#9/16確認数
+cluster = 21
+
+for i in range(cluster):
+    df_cluster = pd.DataFrame.from_dict({
+        '拠点':['工場（静岡県牧之原市）'],
+        '陽性確定':['2021年9月16日'],
+        '工場稼働への影響': ['あり'],
+        '特記事項':['クラスター']
+        }
+    )
+
+    suzuki_covid2 = pd.concat([suzuki_covid2, df_cluster], axis=0)
+
+
+#陽性確定日をDatetime型で格納したカラムを新規作成
+suzuki_covid2['陽性確定日'] = pd.to_datetime(suzuki_covid2['陽性確定'],  format='%Y年%m月%d日')
+suzuki_covid3 = suzuki_covid2[['拠点','陽性確定日']]
+
+#拠点名称のリストを取得
+places = suzuki_covid3['拠点'].unique()
+
+#拠点ごとの累計感染者数を集計
+cumsum_data = pd.DataFrame()
+for place in places:
+    temp = suzuki_covid3[suzuki_covid3['拠点'] == place].groupby('陽性確定日').count().sort_values('陽性確定日').cumsum().rename(columns={'拠点': place})
+    cumsum_data = pd.concat([cumsum_data, temp], axis=1)
+
+
+
+#####
+#ここからstreamlit表示内容
+#####
+st.title('スズキ株式会社　コロナ感染者数集計')
+st.text('出典: スズキ株式会社HP内「当社における新型コロナウイルス感染者の発生について」')
+st.text('参照URL: https://www.suzuki.co.jp/release/d/2020/covid19/')
+st.text('注意: 掲載データは作成者が独自に集計したものでありスズキ株式会社の公式発表ではありません')
+
+add_checkbox = st.sidebar.multiselect(
+    '表示対象とする事業所を選択',
+    places.tolist(),
+    places.tolist()
+)
+
+output_data = suzuki_covid2[suzuki_covid2['拠点'].isin(add_checkbox)]
+
+start_date = st.sidebar.date_input(
+    '集計期間の起点を選択',
+    datetime.datetime.today() - datetime.timedelta(days=60)
+)
+
+end_date = st.sidebar.date_input(
+    '集計期間の終点を選択',
+    datetime.date.today()
+)
+
+fig = px.bar(
+    data_frame = output_data.groupby(['陽性確定日','拠点'], as_index=False).count()[['陽性確定日','拠点','陽性確定']].sort_values('陽性確定日'),
+    x = '陽性確定日',
+    y = '陽性確定',
+    color = '拠点',
+    labels = {'陽性確定':'陽性者数(人)'},
+    range_x = [start_date, end_date] #デフォルトは直近60日間
+)
+
+st.plotly_chart(fig)
